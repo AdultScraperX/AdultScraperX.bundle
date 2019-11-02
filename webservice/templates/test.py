9ac7c7f114efd1f8ef46e90996232eb5cd30a30f
BH1NBN <30344386@qq.com>
2019-11-02 22:59:33 +0800

    Revert "插件 init 解决无法调用 update函数"
    This reverts commit 18d4e6463d56017641df55d5a656a344ded8d1a8.

--- webservice/templates/test.py
+++ webservice/templates/test.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
-import base64
 import re
 
 import config as CONFIG
-base64.b64decode('QWJWIDKWMQ=='.replace('[s]', '/')).decode("utf-8")
+
+for template in CONFIG.SOURCE_LIST['censored']:
+    # 循环模板列表
+    codeList = re.findall(re.compile(template['pattern']),
+                          'Ipz 911 New Spermania 大量口内射精!大量顔面ぶっかけ!オナ禁した男達の特濃ザーメン群がビュンビュン降り注ぐ! 桜木凛')
+    if len(codeList) == 0:
+        break
+    for webSiteClass in template['webList']:
+        webSite = webSiteClass()
+        items = webSite().search("q")
+        for item in items:
+            if item['issuccess']:
+                items.update({'issuccess': 'true'})
+                items['json_data'].append({webSite().getName(): item['data']})
+                print("match=" + '')
